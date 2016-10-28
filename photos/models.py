9fbc7e3fe296b0d0f5fdd94ee9c3f4c046371bfd
Eric Turner <eturner@gamingarts.com>
2016-10-28 16:00:30 -0700

    Use Cloudinary Field for image storage

--- photos/models.py
+++ photos/models.py
@@ -1,31 +1,31 @@
-import uuid
-
+import cloudinary as cloudinary
+from cloudinary.models import CloudinaryField
 from django.db import models
 from django.utils.datetime_safe import datetime
+import cloudinary.api
 
 
 class Photo(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, editable=False)
     lat = models.FloatField(max_length=255, blank=False, null=False)
     long = models.FloatField(max_length=255, blank=False, null=False)
-    original = models.ImageField(max_length=1000, blank=False, null=False)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    thumbnail = models.ImageField(max_length=1000, blank=False, null=False)
     created = models.DateTimeField(editable=False)
+    original = CloudinaryField('original')
 
     def save(self, **kwargs):
         if not self.created:
             self.created = datetime.now()
+            self.id = self.original.public_id
         super(Photo, self).save()
 
     def self_destruct(self):
         """Determine if photo should be deleted"""
         pass
 
-    def make_thumbnail(self):
-        """Automaticlly create thumbnail of self"""
-        pass
-
     def location(self):
         """Return latitude and longitude Tuple"""
-        return self.lat, self.lng
+        return self.lat, self.long
+
+    def delete(self, using=None, keep_parents=False):
+        cloudinary.api.delete_resources(public_ids=[self.original.public_id])
+        return super(Photo, self).delete()
